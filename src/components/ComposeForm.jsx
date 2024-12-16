6053431455d1431ea7d44b9b46f06a112f88fa72
Deontre Fleming <deontre.fleming@hotmail.com>
2024-12-16 14:16:09 -0600

    Add useState to ComposeForm

--- src/components/ComposeForm.jsx
+++ src/components/ComposeForm.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Avatar from "./Avatar";
 import './ComposeForm.css';
 
 function ComposeForm() {
+    const [editorValue, setEditorValue] = useState('');
+    console.log(editorValue);
+    const handleEditorValueChange = (e) => {
+        setEditorValue(e.target.value);
+    }
+
     return (
         <form className="compose-form">
             <div className="compose-form-container">
                 <Avatar />
-                <textarea 
+                <textarea
+                    value={editorValue}
+                    onChange={handleEditorValueChange}
                     className="compose-form-textarea"
                     placeholder="What's happening?"
                 />
