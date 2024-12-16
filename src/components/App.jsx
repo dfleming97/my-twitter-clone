aa55e3de48a78d518149706ab8c42aff398bf683
Deontre Fleming <deontre.fleming@hotmail.com>
2024-12-16 14:40:00 -0600

    useState, nanoid to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import ComposeForm from './ComposeForm';
 import Timeline from './Timeline';
 import { FaXRay } from 'react-icons/fa';
 import './App.css';
 
-import tweets from '../tweets.json';
+import initialTweets from '../tweets.json';
+
+const CURRENT_USER = 'deetray';
 
 
 function App() {
+    const [tweets, setTweets] =useState(initialTweets);
+
+    const handlePostTweet = (content) => {
+        const newTweet = {
+            content,
+            id: nanoid(),
+            created_on: Date(Date.now()),
+            user: CURRENT_USER,
+            comments_count: 0,
+            retweets_count: 0,
+            favorites_count: 0,
+        }
+        setTweets([...tweets, newTweet])
+    }
+
     return (
         <div className='app'>
             <FaXRay className='app-logo' size="2em" />
-            <ComposeForm />
+            <ComposeForm onSubmit={handlePostTweet} />
            <div className='separator'></div>
            <Timeline tweets={tweets} />
         </div>
