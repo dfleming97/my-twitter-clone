fb829bbd1d5564b85a45129b1e1737b0311a9770
Deontre Fleming <deontre.fleming@hotmail.com>
2024-12-16 12:23:16 -0600

    added tweets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,15 @@ import tweets from '../tweets.json';
 
 function App() {
     return (
-        <div>
-            {tweets.map((tweet) => (
-                <span>tweet.content</span>
+        <div className='timeline'>
+            {tweets.map(({ id, user, created_on, content }) => (
+                <div key={id} className='tweet'>
+                    <div className='tweet-header'>
+                        <span className='tweet-user'>@{user}</span> ·{' '}
+                        <span className='tweet-created-on'>{created_on}</span>
+                    </div>
+                    <div className='tweet-content'>{content}</div>
+                </div>
             ))}
         </div>
     )
