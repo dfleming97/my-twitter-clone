cb48d4636f5a6fcceb1aeaeaebe455794d91eda5
Deontre Fleming <deontre.fleming@hotmail.com>
2024-12-16 12:31:25 -0600

    tweet component added

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,15 @@
+function Tweet(props) {
+    const { user, createdOn, children } = props;
+
+    return (
+        <div className="tweet">
+            <div className="tweet-header">
+                <span className="tweet-user">@{user}</span> ·{' '}
+                <span className="tweet-created-on">{createdOn}</span>
+            </div>
+            <div className="tweet-content">{children}</div>
+        </div>
+    )
+}
+
+export default Tweet;
